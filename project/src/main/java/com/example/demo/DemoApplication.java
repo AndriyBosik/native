40ba2e52e9372ee4442a5e26bd1addd08e1d43e1
Andrii Bosyk <andriybosik2002@ukr.net>
2024-06-02 23:05:14 +0300

    Minor fix

--- project/src/main/java/com/example/demo/DemoApplication.java
+++ project/src/main/java/com/example/demo/DemoApplication.java
@@ -1,17 +1,15 @@
 package com.example.demo;
 
 import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.internal.servlet.AwsLambdaServletContainerHandler;
 import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spring.SpringBootAwsProxyExceptionHandler;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.serverless.proxy.spring.SpringBootProxyHandlerBuilder;
-import com.amazonaws.serverless.proxy.spring.SpringDelegatingLambdaContainerHandler;
 import com.example.demo.config.MyRuntimeHints;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ImportRuntimeHints;
 
@@ -36,13 +34,12 @@ public class DemoApplication {
         SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler = new SpringBootProxyHandlerBuilder<AwsProxyRequest>()
                 .defaultProxy()
                 .springBootApplication(DemoApplication.class)
+                .servletApplication()
                 .buildAndInitialize();
-        if (args.length == 0) {
-            return;
+        if (args.length > 0) {
+            InputStream inputStream = new ByteArrayInputStream(args[0].getBytes());
+            OutputStream outputStream = new FileOutputStream("temp1.txt");
+            handler.proxyStream(inputStream, outputStream, null);
         }
-
-        InputStream inputStream = new ByteArrayInputStream(args[0].getBytes());
-        OutputStream outputStream = new FileOutputStream("temp1.txt");
-        handler.proxyStream(inputStream, outputStream, null);
     }
 }
