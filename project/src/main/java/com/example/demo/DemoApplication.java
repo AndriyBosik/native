aae240716e93f3cf8f3f4a7a73389b2bb4a73cf6
Andrii Bosyk <andriybosik2002@ukr.net>
2024-06-02 20:23:51 +0300

    Minor fix

--- project/src/main/java/com/example/demo/DemoApplication.java
+++ project/src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,10 @@
 package com.example.demo;
 
-import com.example.demo.config.LambdaHandler;
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.serverless.proxy.spring.SpringBootProxyHandlerBuilder;
 import com.example.demo.config.MyRuntimeHints;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportRuntimeHints;
@@ -14,13 +18,26 @@ import java.io.OutputStream;
 @SpringBootApplication
 @ImportRuntimeHints(MyRuntimeHints.class)
 public class DemoApplication {
+    private static final SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+
+    static {
+        try {
+            handler = new SpringBootProxyHandlerBuilder<AwsProxyRequest>()
+                    .defaultProxy()
+                    .springBootApplication(DemoApplication.class)
+                    .buildAndInitialize();
+        } catch (
+                ContainerInitializationException exception) {
+            throw new RuntimeException("Could not initialize Spring Boot application", exception);
+        }
+    }
+
     public static void main(String[] args) throws IOException {
-        LambdaHandler handler = new LambdaHandler();
         if (args.length == 0) {
             return;
         }
         InputStream inputStream = new ByteArrayInputStream(args[0].getBytes());
         OutputStream outputStream = new FileOutputStream("temp1.txt");
-        handler.handleRequest(inputStream, outputStream, null);
+        handler.proxyStream(inputStream, outputStream, null);
     }
 }
--- project/src/main/java/com/example/demo/config/MyRuntimeHints.java
+++ project/src/main/java/com/example/demo/config/MyRuntimeHints.java
@@ -10,7 +10,6 @@ public class MyRuntimeHints implements RuntimeHintsRegistrar {
     public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
         try {
             hints.reflection()
-                    .registerType(LambdaHandler.class)
                     .registerConstructor(ParameterNamesModule.class.getDeclaredConstructor(), ExecutableMode.INVOKE);
         } catch (NoSuchMethodException exception) {
             throw new RuntimeException(exception);
