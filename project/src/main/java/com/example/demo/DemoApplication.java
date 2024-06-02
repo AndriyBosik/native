0fbbfd0e02daa5b61c686c3cd1ad50c8b4edc5b4
Andrii Bosyk <andriybosik2002@ukr.net>
2024-06-02 16:00:06 +0300

    Minor fix

--- project/src/main/java/com/example/demo/DemoApplication.java
+++ project/src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
+import com.example.demo.config.MyRuntimeHints;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
+@ImportRuntimeHints(MyRuntimeHints.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
