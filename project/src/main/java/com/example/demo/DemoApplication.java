c92898bb9dc6266590e62b445b194087ab42cde4
Andrii Bosyk <andriybosik2002@ukr.net>
2024-06-02 20:14:17 +0300

    Improve LambdaHandler

--- project/src/main/java/com/example/demo/DemoApplication.java
+++ project/src/main/java/com/example/demo/DemoApplication.java
@@ -1,16 +1,23 @@
 package com.example.demo;
 
+import com.example.demo.config.LambdaHandler;
 import com.example.demo.config.MyRuntimeHints;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportRuntimeHints;
+
+import java.io.ByteArrayInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 @SpringBootApplication
 @ImportRuntimeHints(MyRuntimeHints.class)
 public class DemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+    public static void main(String[] args) throws IOException {
+        LambdaHandler handler = new LambdaHandler();
+        InputStream inputStream = new ByteArrayInputStream(args[0].getBytes());
+        OutputStream outputStream = new FileOutputStream("temp1.txt");
+        handler.handleRequest(inputStream, outputStream, null);
+    }
 }
