723a821fb33522f17b1a79a0dbe8f54d92b0fe36
Andrii Bosyk <andriybosik2002@ukr.net>
2024-06-02 20:42:14 +0300

    Minor fix

--- project/src/main/java/com/example/demo/DemoApplication.java
+++ project/src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,6 @@ import com.amazonaws.serverless.exceptions.ContainerInitializationException;
 import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
-import com.amazonaws.serverless.proxy.spring.SpringBootProxyHandlerBuilder;
 import com.example.demo.config.MyRuntimeHints;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,27 +18,16 @@ import java.io.OutputStream;
 @SpringBootApplication
 @ImportRuntimeHints(MyRuntimeHints.class)
 public class DemoApplication {
-//    private static final SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
-//
-//    static {
-//        try {
-//            handler = new SpringBootProxyHandlerBuilder<AwsProxyRequest>()
-//                    .defaultProxy()
-//                    .springBootApplication(DemoApplication.class)
-//                    .buildAndInitialize();
-//        } catch (
-//                ContainerInitializationException exception) {
-//            throw new RuntimeException("Could not initialize Spring Boot application", exception);
-//        }
-//    }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ContainerInitializationException {
         SpringApplication.run(DemoApplication.class, args);
-//        if (args.length == 0) {
-//            return;
-//        }
-//        InputStream inputStream = new ByteArrayInputStream(args[0].getBytes());
-//        OutputStream outputStream = new FileOutputStream("temp1.txt");
-//        handler.proxyStream(inputStream, outputStream, null);
+        SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler = SpringBootLambdaContainerHandler
+                .getAwsProxyHandler(DemoApplication.class);
+        if (args.length == 0) {
+            return;
+        }
+        InputStream inputStream = new ByteArrayInputStream(args[0].getBytes());
+        OutputStream outputStream = new FileOutputStream("temp1.txt");
+        handler.proxyStream(inputStream, outputStream, null);
     }
 }
