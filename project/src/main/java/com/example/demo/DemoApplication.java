12803a0e4b345d8dea97bf3dfd117b08dbcce5b2
Andrii Bosyk <andriybosik2002@ukr.net>
2024-06-03 21:23:56 +0300

    Add small log

--- project/src/main/java/com/example/demo/DemoApplication.java
+++ project/src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import com.example.demo.config.MyRuntimeHints;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportRuntimeHints;
@@ -8,7 +10,10 @@ import org.springframework.context.annotation.ImportRuntimeHints;
 @SpringBootApplication
 @ImportRuntimeHints(MyRuntimeHints.class)
 public class DemoApplication {
+    private static final Log log = LogFactory.getLog(DemoApplication.class);
+
     public static void main(String[] args) {
+        log.info("started");
         SpringApplication.run(DemoApplication.class, args);
     }
 }
