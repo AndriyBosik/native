ee27d60f2179731ca6a99feb09bf42def26edf6c
Andrii Bosyk <andriybosik2002@ukr.net>
2024-06-03 20:35:43 +0300

    Minor fix

--- project/src/main/java/com/example/demo/DemoApplication.java
+++ project/src/main/java/com/example/demo/DemoApplication.java
@@ -1,39 +1,14 @@
 package com.example.demo;
 
-import com.amazonaws.serverless.exceptions.ContainerInitializationException;
 import com.example.demo.config.MyRuntimeHints;
-import com.example.demo.config.aws.NativeLambdaContainerHandler;
-import com.example.demo.config.aws.NativeLambdaContainerHandlerBuilder;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportRuntimeHints;
-
-import java.io.ByteArrayInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 @SpringBootApplication
 @ImportRuntimeHints(MyRuntimeHints.class)
 public class DemoApplication {
-    public static void main(String[] args) throws IOException, ContainerInitializationException {
-//        if (args.length == 0) {
-//            SpringApplication.run(DemoApplication.class, args);
-//            return;
-//        }
-
-//        ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
-
-        System.out.println(System.getenv("AWS_LAMBDA_INITIALIZATION_TYPE"));
-        NativeLambdaContainerHandler handler = new NativeLambdaContainerHandlerBuilder()
-                .defaultProxy()
-                .springBootApplication(DemoApplication.class)
-                .servletApplication()
-                .buildAndInitialize();
-        if (args.length > 0) {
-            InputStream inputStream = new ByteArrayInputStream(args[0].getBytes());
-            OutputStream outputStream = new FileOutputStream("temp1.txt");
-            handler.proxyStream(inputStream, outputStream, null);
-        }
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
     }
 }
